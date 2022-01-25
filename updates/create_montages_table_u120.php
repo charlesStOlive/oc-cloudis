ae1bca1d34f134c81ed40696ffbd015f7ff70e77
Charles <charles.stolive@gmail.com>
2022-01-25 08:52:30 +0100

    Ajout du champs mémo

--- updates/create_montages_table_u120.php
+++ updates/create_montages_table_u120.php
@@ -4,17 +4,19 @@ use Winter\Storm\Database\Schema\Blueprint;
 use Winter\Storm\Database\Updates\Migration;
 use Schema;
 
-class CreateMontagesTableU110 extends Migration
+class CreateMontagesTableU120 extends Migration
 {
     public function up()
     {
         Schema::table('waka_cloudis_montages', function (Blueprint $table) {
+            $table->text('memo')->nullable();
         });
     }
 
     public function down()
     {
         Schema::table('waka_cloudis_montages', function (Blueprint $table) {
+            $table->dropColumn('memo');
         });
     }
 }
